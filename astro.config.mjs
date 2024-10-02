b061af02e53a976bdfcc677661e259fc3c92c3fa
Alfian Azizi <azizi@kirei.co.id>
2024-10-02 15:44:15 +0700

    remove unused Window

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	output: 'hybrid',
 	site: "https://alfianazizi.github.io",
 	integrations: [mdx(), sitemap()],
 	vite: {
